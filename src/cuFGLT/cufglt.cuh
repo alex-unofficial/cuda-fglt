0dc5d75bde63ef60e6641ab248edc51a372b903f
alex-unofficial <athanasiadisalex2@gmail.com>
2023-02-28 04:22:46 +0200

    added licence stubs

--- src/cuFGLT/cufglt.cuh
+++ src/cuFGLT/cufglt.cuh
@@ -1,3 +1,22 @@
+/* header file for FGLT with CUDA
+ * Copyright (C) 2023  Alexandros Athanasiadis
+ *
+ * This file is part of cuda-fglt
+ *                                                                        
+ * cuda-fglt is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *                                                                        
+ * cuda-fglt is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *                                                                        
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>. 
+ */
+
 #ifndef CUFGLT_CUH
 #define CUFGLT_CUH
 
--- tools/cuda-fglt.sh
+++ tools/cuda-fglt.sh
@@ -1,3 +1,21 @@
+# BATCH script for testing on HPC
+# Copyright (C) 2023  Alexandros Athanasiadis
+# 
+# This file is part of cuda-fglt
+#                                                                        
+# cuda-fglt is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#                                                                        
+# cuda-fglt is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#                                                                        
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>. 
+
 #!/bin/bash
 #SBATCH --partition=gpu
 #SBATCH --time=0:30:00
